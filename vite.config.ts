8c705176d886ff9298d2f6e10a02a60fac4a8a07
Sergey Kostin <kostinss@me.com>
2024-01-22 14:52:09 +0300

    GHP 15

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: { port: 3000 },
   base: "/RIP-WeStatsApp/",
-  publicDir: "public",
+  //publicDir: "public",
   plugins: [react()],
 })
