f8ad40c8bf838a4cd47d33c28fa166e6ffd26bff
Sergey Kostin <kostinss@me.com>
2024-01-22 15:13:18 +0300

    GHP -18 404 Rollback

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,4 @@ export default defineConfig({
   //server: { port: 3000 },
   base: "/RIP-WeStatsApp/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "public/404.html"),
-      },
-    },
-  },
 })
